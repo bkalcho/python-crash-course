48e2ff69543d24ddc32dbc4f34a68d616854c838
Bojan Kalicanin <bojan.g.kalicanin@gmail.com>
2016-10-11 18:47:26 +0200

    Added TestCase with unit tests that test a class

---
+++ survey.py
@@ -0,0 +1,25 @@
+# Author: Bojan G. Kalicanin
+# Date: 11-Oct-2016
+# Class that models surveys
+
+class AnonymousSurvey():
+    """Collect anonymous answers to a survey question."""
+    
+    def __init__(self, question):
+        """Store a question, and prepare to store responses."""
+        self.question = question
+        self.responses = []
+        
+    def show_question(self):
+        """Show survey question."""
+        print(self.question)
+        
+    def store_response(self, new_response):
+        """Store a single response to the survey."""
+        self.responses.append(new_response)
+        
+    def show_results(self):
+        """Show all the responses that have been given."""
+        print("Survey results:")
+        for response in self.responses:
+            print('- ' + response)
