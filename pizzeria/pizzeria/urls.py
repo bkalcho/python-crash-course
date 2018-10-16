61d59dc8182ad61edc81e6702a2b343a52d434bd
BryanFondrick <BryanF1031@aol.com>
2018-10-16 10:35:37 -0700

    Update urls.py
    Changed to use path  and include instead of url, removed reg. expressions

--- pizzeria/pizzeria/urls.py
+++ pizzeria/pizzeria/urls.py
@@ -1,22 +1,24 @@
 """pizzeria URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.contrib import admin
 
+
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('pizzas.urls', namespace='pizzas')),
+    path('admin/', admin.site.urls),
+    
+    path('', include('pizzas.urls')),
 ]
